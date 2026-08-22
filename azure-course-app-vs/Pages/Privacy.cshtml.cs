727c9150d9ff31cf48ed766ae5c36f3bf4716303
Mohamad Alsokromy <sokrom1996@gmail.com>
2026-08-22 11:41:57 +0300

    Add project files.

---
+++ azure-course-app-vs/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_course_app_vs.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
